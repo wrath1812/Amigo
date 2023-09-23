f8644ea38f38c42509ecc92552b7bfe95b7c2e2d
Bhaumik-Tandan <bhaumik.tandan@gmail.com>
2023-09-23 12:27:20 +0530

    [ADD] secure storage and local storage

---
+++ helper/localStorage.js
@@ -0,0 +1,38 @@
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+export const setLocalStoreData = async (
+    objKey,
+    value
+) => {
+    try {
+        if (!objKey) {
+            return { status: false };
+        }
+        const jsonValue = JSON.stringify(value);
+        return await AsyncStorage.setItem(objKey, jsonValue);
+    } catch (error) {
+        return { status: false, error };
+    }
+};
+export const removeLocalStoreData = async (objKey) => {
+    try {
+        if (!objKey) {
+            return { status: false };
+        }
+        return AsyncStorage.removeItem(objKey);
+    } catch (error) {
+        return { status: false, error };
+    }
+};
+
+export const getLocalStoreData = async (key) => {
+    try {
+        const jsonValue = await AsyncStorage.getItem(key);
+        return jsonValue != null ? JSON.parse(jsonValue) : null;
+    } catch (error) {
+        return error;
+        // error reading value
+    }
+};
+
+export default { setLocalStoreData, getLocalStoreData, removeLocalStoreData };
---
+++ helper/secureStorage.js
@@ -0,0 +1,26 @@
+import * as SecureStore from 'expo-secure-store';
+
+async function storeSecret(key, secret) {
+  try {
+    await SecureStore.setItemAsync(key, secret);
+    console.log('Secret stored successfully.');
+  } catch (error) {
+    console.error('Error storing secret:', error);
+  }
+}
+
+async function retrieveSecret(key) {
+  try {
+    const secret = await SecureStore.getItemAsync(key);
+    if (secret) {
+        return secret;
+    } else {
+        return null;
+    }
+  } catch (error) {
+    console.error('Error retrieving secret:', error);
+  }
+}
+
+export { storeSecret, retrieveSecret };
+
