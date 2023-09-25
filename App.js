f396acd51815fe6f092a85d3b9e13f6965b50207
Bhaumik-Tandan <bhaumik.tandan@gmail.com>
2023-09-26 00:06:49 +0530

    [ADD] sentry

--- App.js
+++ App.js
@@ -4,7 +4,17 @@ import { Platform } from 'react-native';
 import Constants from 'expo-constants';
 import RootNavigator from './navigator/RootNavigator';
 import { AuthProvider } from './context/AuthContext';
-export default function App() {
+import * as Sentry from "@sentry/react-native";
+
+Sentry.init({
+    dsn: "https://458aa34234bdfb742494c836127ddf3e@o4505941411495936.ingest.sentry.io/4505941423554560",
+    enableInExpoDevelopment: true,
+    enableAutoPerformanceTracking: true,
+    enableAutoSessionTracking: true,
+    environment: 'development',
+    debug: 'development',
+});
+function App() {
     return (
         <SafeAreaProvider
             style={{
@@ -20,4 +30,5 @@ export default function App() {
     );
 }
 
+export default Sentry.wrap(App);
 // https://stackoverflow.com/questions/72768/how-do-you-detect-credit-card-type-based-on-number
