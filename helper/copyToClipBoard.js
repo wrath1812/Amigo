c7217b236874da889ec2922ce507fc4ec1a945cd
Bhaumik-Tandan <bhaumik.tandan@gmail.com>
2023-10-03 17:35:17 +0530

    [ADD] confirmation box

---
+++ helper/copyToClipBoard.js
@@ -0,0 +1,10 @@
+import * as Clipboard from 'expo-clipboard';
+import Toast from 'react-native-root-toast';
+const copyToClipBoard = (text,message) => {
+    Clipboard.setString(text);
+    Toast.show(message, {
+        duration: Toast.durations.LONG,
+    });
+};
+
+export default copyToClipBoard;
