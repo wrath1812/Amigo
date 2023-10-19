567952df301d02a4d943ff8f5582b5045aabafdf
Bhaumik-Tandan <bhaumik.tandan@gmail.com>
2023-10-19 15:33:16 +0530

    [SEPERATE] card compnent in main page and add card page

--- components/CardBox.js
+++ components/CardBox.js
@@ -1,16 +1,20 @@
 import React, { useState } from 'react';
-import { View, StyleSheet, TouchableOpacity, Text } from 'react-native';
+import { View, StyleSheet, TouchableOpacity, Text,Image } from 'react-native';
 import Ionicons from '@expo/vector-icons/Ionicons';
 import { useAuth } from '../context/AuthContext';
 import { getLocalStoreData, setLocalStoreData } from '../helper/localStorage';
 import { CARDS } from '../constants/string';
-import { calcHeight, calcWidth } from '../helper/res';
+import { calcHeight, calcWidth,getFontSizeByWindowWidth } from '../helper/res';
 import copyToClipBoard from '../helper/copyToClipBoard';
 import Card from './card';
 import DeleteCardModal from './DeleteCardModal';
 import Modal from 'react-native-modal';
 import AddCardModal from './AddCardModal';
 import CardMenu from './CardMenu';
+import CARD_ICON from '../constants/cardIcon';
+import CARD_COLOR from '../constants/cardColour';
+import MASK_COLORS from '../constants/maskColour';
+import formatCardNumber from './formatCardNumber';
 
 import getEncryptionKey from '../util/getEncryptionKey';
 import { encryptData } from '../helper/encryption';
@@ -21,6 +25,7 @@ function CardBox({ item }) {
     const [showMenu, setShowMenu] = useState(false);
     const [showCard, setShowCard] = useState(false);
     const [showEditCard, setShowEditCard] = useState(false);
+    const [showCVV, setShowCVV] = useState(false);
 
     async function deleteCard() {
         const encryptedCards = await getLocalStoreData(CARDS);
@@ -78,7 +83,99 @@ function CardBox({ item }) {
     return (
         <View style={styles.container}>
             {/* Render the Card component directly with the showCard prop */}
-            <Card item={item} showCard={showCard} />
+            <View
+            style={{
+                ...styles.card,
+                backgroundColor: CARD_COLOR[item.type] || item.color,
+            }}
+        >
+            <View
+                style={{
+                    flexDirection: 'row',
+                    justifyContent: 'space-between',
+                    alignItems: 'center',
+                }}
+            >
+                <Text
+                    style={{
+                        ...styles.cardText,
+                        textAlign: 'auto',
+                        fontSize: getFontSizeByWindowWidth(12),
+                        fontWeight: 'bold',
+                    }}
+                >
+                    {item.nickname}
+                </Text>
+                <TouchableOpacity onPress={() => {/* Add your menu logic here */}}>
+                    <Ionicons
+                        name="ellipsis-vertical-outline"
+                        size={calcHeight(4)}
+                        color="black"
+                    />
+                </TouchableOpacity>
+            </View>
+
+            <View style={{
+                alignItems: 'flex-end',
+                marginTop: calcHeight(1),
+                marginRight: calcWidth(1),
+            }}>
+                <Image
+                    source={CARD_ICON[item.type]}
+                    style={{ width: calcWidth(15), height: calcHeight(5) }}
+                />
+            </View>
+
+            <View>
+                {formatCardNumber(item.card_number, showCVV , MASK_COLORS[item.type])}
+            </View>
+
+            <Text
+                style={{
+                    ...styles.cardText,
+                    textAlign: 'auto',
+                    fontSize: getFontSizeByWindowWidth(12),
+                }}
+            >
+                {item.name_on_card}
+            </Text>
+
+            <View style={styles.cardDetailsContainer}>
+                <View>
+                    <Text style={styles.cardLabelText}>Valid Thru</Text>
+                    <Text style={styles.cardText}>
+                        {item.expiry}
+                    </Text>
+                </View>
+                <View>
+                    <TouchableOpacity
+                        style={styles.button}
+                        onPress={() => setShowCVV((prev) => !prev)}
+                    >
+                        <Ionicons
+                            name={showCVV ? 'eye' : 'eye-off'}
+                            size={calcHeight(4)}
+                            color="black"
+                        />
+                    </TouchableOpacity>
+                </View>
+                <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+                    <View>
+                        <Text style={styles.cardLabelText}>CVV</Text>
+                        {item.cvv && (
+                            <Text style={styles.cardText}>
+                                {showCVV ? item.cvv : 'XXX'}
+                            </Text>
+                        )}
+                    </View>
+                    {!showCVV && (
+                        <View style={styles.cvvMaskOverlay}>
+                            <Text style={styles.cvvMaskText}>Tap to Reveal</Text>
+                        </View>
+                    )}
+                </View>
+            </View>
+        </View>
 
             {/* Add the three buttons */}
             <View style={styles.buttonBar}>
@@ -170,5 +267,47 @@ const styles = StyleSheet.create({
     modal: {
         justifyContent: 'flex-end',
         margin: 0,
+    },
+    card: {
+        padding: calcHeight(3),
+        borderRadius: calcHeight(2),
+        margin: calcHeight(1),
+        elevation: 3,
+        width: calcWidth(90),
+    },
+    cardText: {
+        color: 'white',
+        fontSize: getFontSizeByWindowWidth(10),
+        textAlign: 'center',
+    },
+    cardDetailsContainer: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        marginTop: calcHeight(1),
+    },
+    cardLabelText: {
+        color: 'white',
+        fontSize: getFontSizeByWindowWidth(8),
+        opacity: 0.6,
+    },
+    cvvMaskOverlay: {
+        backgroundColor: 'rgba(0, 0, 0, 0.6)', // Semi-transparent background
+        position: 'absolute',
+        top: 0,
+        left: 0,
+        width: '100%',
+        height: '100%',
+        zIndex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    cvvMaskText: {
+        color: 'white',
+        fontSize: getFontSizeByWindowWidth(12),
+    },
+    button: {
+        zIndex: 1,
+        position: 'absolute',
+        left: 0,
     },
 });
--- components/card.js
+++ components/card.js
@@ -1,156 +1,92 @@
 import React, { useState } from 'react';
-import { View, Text, StyleSheet, Image, TouchableOpacity } from 'react-native';
-import { Ionicons } from '@expo/vector-icons';
+import { View, Text, StyleSheet, TouchableOpacity,  Image } from 'react-native';
+import * as Clipboard from 'expo-clipboard';
+import Ionicons from '@expo/vector-icons/Ionicons';
 import CARD_ICON from '../constants/cardIcon';
 import CARD_COLOR from '../constants/cardColour';
-import MASK_COLORS from '../constants/maskColour';
-import formatCardNumber from './formatCardNumber';
+import Toast from 'react-native-root-toast';
 
-import { calcHeight, getFontSizeByWindowWidth, calcWidth } from '../helper/res';
 
+
+
+function formatCardNumber(cardNumber) {
+  return cardNumber.replace(/(.{4})/g, '$1    ');
+}
+
+function maskCardNumber(cardNumber) {
+  const visibleDigits = 4;
+  const maskedSection = cardNumber.slice(0, -visibleDigits);
+  const visibleSection = cardNumber.slice(-visibleDigits);
+  return maskedSection.replace(/./g, '*') + visibleSection;
+}
+
+function getCardNumberDisplayValue(cardNumber, showFullNumber) {
+  if (!showFullNumber) 
+   cardNumber = maskCardNumber(cardNumber);
+  
+  return formatCardNumber(cardNumber);
+}
 function Card({ item }) {
-    const [showCVV, setShowCVV] = useState(false);
+  const [showCVV, setShowCVV] = useState(false);
+  const handleClipboardPress = async () => {
+    Clipboard.setString(item.card_number);
+    Toast.show('Card Number Copied to Clipboard', {
+      duration: Toast.durations.LONG,
+  });
 
-    return (
-        <View
-            style={{
-                ...styles.card,
-                backgroundColor: CARD_COLOR[item.type] || item.color,
-            }}
-        >
-            <View
-                style={{
-                    flexDirection: 'row',
-                    justifyContent: 'space-between',
-                    alignItems: 'center',
-                }}
-            >
-                <Text
-                    style={{
-                        ...styles.cardText,
-                        textAlign: 'auto',
-                        fontSize: getFontSizeByWindowWidth(12),
-                        fontWeight: 'bold',
-                    }}
-                >
-                    {item.nickname}
-                </Text>
-                <TouchableOpacity onPress={() => {/* Add your menu logic here */}}>
-                    <Ionicons
-                        name="ellipsis-vertical-outline"
-                        size={calcHeight(4)}
-                        color="black"
-                    />
-                </TouchableOpacity>
-            </View>
+  }; 
 
-            <View style={{
-                alignItems: 'flex-end',
-                marginTop: calcHeight(1),
-                marginRight: calcWidth(1),
-            }}>
-                <Image
-                    source={CARD_ICON[item.type]}
-                    style={{ width: calcWidth(15), height: calcHeight(5) }}
-                />
-            </View>
-
-            <View>
-                {formatCardNumber(item.card_number, showCVV , MASK_COLORS[item.type])}
-            </View>
-
-            <Text
-                style={{
-                    ...styles.cardText,
-                    textAlign: 'auto',
-                    fontSize: getFontSizeByWindowWidth(12),
-                }}
-            >
-                {item.name_on_card}
-            </Text>
-
-            <View style={styles.cardDetailsContainer}>
-                <View>
-                    <Text style={styles.cardLabelText}>Valid Thru</Text>
-                    <Text style={styles.cardText}>
-                        {item.expiry}
-                    </Text>
-                </View>
-                <View>
-                    <TouchableOpacity
-                        style={styles.button}
-                        onPress={() => setShowCVV((prev) => !prev)}
-                    >
-                        <Ionicons
-                            name={showCVV ? 'eye' : 'eye-off'}
-                            size={calcHeight(4)}
-                            color="black"
-                        />
-                    </TouchableOpacity>
-                </View>
-                <View style={{ flexDirection: 'row', alignItems: 'center' }}>
-                    <View>
-                        <Text style={styles.cardLabelText}>CVV</Text>
-                        {item.cvv && (
-                            <Text style={styles.cardText}>
-                                {showCVV ? item.cvv : 'XXX'}
-                            </Text>
-                        )}
-                    </View>
-                    {!showCVV && (
-                        <View style={styles.cvvMaskOverlay}>
-                            <Text style={styles.cvvMaskText}>Tap to Reveal</Text>
-                        </View>
-                    )}
-                </View>
-            </View>
+  return (
+    <View style={{ ...styles.card, backgroundColor: CARD_COLOR[item.type] || item.color}}>
+      <View  style={{flexDirection: 'row', justifyContent: 'space-between'}}>
+      <Text style={styles.cardText}>{item.nickname}</Text>
+      <Image source={CARD_ICON[item.type] } style={{ width: 50, height: 50 }} />
+      </View>
+      <Text style={styles.cardNumber}>{getCardNumberDisplayValue(item.card_number,!showCVV)}</Text>
+      <Text style={styles.cardText}>{item.expiry}</Text>
+      <View style={styles.cvvContainer}>
+       {item.cvv&&( <Text style={styles.cardText}>{item.cvv}</Text>)}
+       
+        <View>
+        
         </View>
-    );
+      </View>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    card: {
-        padding: calcHeight(3),
-        borderRadius: calcHeight(2),
-        margin: calcHeight(1),
-        elevation: 3,
-        width: calcWidth(90),
-    },
-    cardText: {
-        color: 'white',
-        fontSize: getFontSizeByWindowWidth(10),
-        textAlign: 'center',
-    },
-    cardDetailsContainer: {
-        flexDirection: 'row',
-        justifyContent: 'space-between',
-        marginTop: calcHeight(1),
-    },
-    cardLabelText: {
-        color: 'white',
-        fontSize: getFontSizeByWindowWidth(8),
-        opacity: 0.6,
-    },
-    cvvMaskOverlay: {
-        backgroundColor: 'rgba(0, 0, 0, 0.6)', // Semi-transparent background
-        position: 'absolute',
-        top: 0,
-        left: 0,
-        width: '100%',
-        height: '100%',
-        zIndex: 1,
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-    cvvMaskText: {
-        color: 'white',
-        fontSize: getFontSizeByWindowWidth(12),
-    },
-    button: {
-        zIndex: 1,
-        position: 'absolute',
-        left: 0,
-    },
-});
+  card: {
+    padding: 16,
+    borderRadius: 10, // Rounded corners
+    margin: 20,
+    elevation: 3, // Shadow for Android
+    shadowColor: 'rgba(0, 0, 0, 0.1)', // Shadow for iOS
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.8,
+    height: 200,
+    width: 300,
+  },
+  cardText: {
+    color: 'white', // Text color
+    fontSize: 16,
+  },
+  cvvContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+  },
+  cardNumber: {
+    color: 'white',
+    fontSize: 17, // Adjust the font size as needed for the card number
+    margin: 5, 
+    fontWeight: 'bold',
+    // horizontally center
+    textAlign: 'center',
+    // add shadow to the bottom
+    textShadowColor: 'rgba(0, 0, 0, 0.75)',
+  },
+}); 
+
 
 export default Card;
