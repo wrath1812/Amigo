2eb6452ef82a21f22c76f456b7061bf3b3a437a0
Bhaumik-Tandan <bhaumik.tandan@gmail.com>
2023-12-23 13:21:04 +0530

    [ADD] code for local storage

--- helper/apiHelper.js
+++ helper/apiHelper.js
@@ -1,5 +1,7 @@
 import axios from 'axios';
 import { API_URL } from '@env';
+import { TOKEN } from '../constants/string';
+import { getLocalStoreData } from './localStorage';
 
 const config = {
     baseURL: API_URL,
@@ -9,5 +11,19 @@ const config = {
     },
 };
 const request = axios.create(config);
+request.interceptors.request.use(
+    async (reqConfig) => {
+        const reqHeader = { ...reqConfig };
+        const token = await getLocalStoreData(TOKEN);
+        if (token) {
+            reqHeader.headers = {
+                ...reqConfig.headers,
+                Authorization: `Bearer ${token}`,
+            };
+        }
 
+        return reqHeader;
+    },
+    (error) => Promise.reject(error),
+);
 export default request;
