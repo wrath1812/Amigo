50e467b1adc04c21e2c81e65a281e7b339912d8a
Bhaumik-Tandan <bhaumik.tandan@gmail.com>
2023-12-23 21:36:50 +0530

    [ADD] code for usefocus effect'

--- pages/GroupListScreen.js
+++ pages/GroupListScreen.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useCallback, useState } from 'react';
 import {
     Text,
     StyleSheet,
@@ -11,19 +11,23 @@ import Loader from '../components/Loader';
 import apiHelper from '../helper/apiHelper';
 import PAGES from '../constants/pages';
 import FabIcon from '../components/FabIcon';
+import { useFocusEffect } from '@react-navigation/native';
 
 function GroupListScreen({ navigation }) {
     const [groups, setGroups] = useState([]);
     const [loading, setLoading] = useState(false);
 
-    useEffect(() => {
-        (async () => {
+    useFocusEffect(
+        useCallback(() => {
+          // Fetch data again when the screen gains focus
+          (async () => {
             setLoading(true);
             const { data } = await apiHelper('/group');
             setGroups(data);
             setLoading(false);
-        })();
-    }, []);
+          })();
+        }, [])
+      );
 
     return loading ? (
         <Loader />
