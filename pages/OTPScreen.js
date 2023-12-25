4bdc25df6e724ea0875fc7346899e40ff8ab5917
Bhaumik-Tandan <bhaumik.tandan@gmail.com>
2023-12-25 20:43:19 +0530

    [ADD] otp screen fix

--- pages/OTPScreen.js
+++ pages/OTPScreen.js
@@ -1,64 +1,60 @@
-import React, { useState, useRef, useEffect } from 'react';
-import { View, Text, TextInput, StyleSheet, SafeAreaView,Image } from 'react-native';
+import React, { useState, useEffect, useRef } from 'react';
+import { View, Text, TextInput, StyleSheet, SafeAreaView, Image, Pressable } from 'react-native';
 import COLOR from '../constants/Colors';
 import Button from '../components/Button';
-import { calcHeight, calcWidth,getFontSizeByWindowWidth } from '../helper/res';
+import { calcHeight, calcWidth, getFontSizeByWindowWidth } from '../helper/res';
 import OTPImage from "../assets/OTPImage.png";
 import PAGES from '../constants/pages';
 
 const OTPScreen = ({ navigation }) => {
-  const [otp, setOtp] = useState(new Array(6).fill(''));
-  const inputRefs = useRef(otp.map(() => React.createRef()));
+  const [otp, setOtp] = useState('');
+  const inputRef = useRef();
 
-  const handleOTPChange = (text, index) => {
-    const newOtp = [...otp];
-    newOtp[index] = text;
-    setOtp(newOtp);
+  useEffect(() => {
+    inputRef.current.focus();
+  }, []);
 
-    // Move focus to the next input if text is entered
-    if (text && index < otp.length - 1) {
-      inputRefs.current[index + 1].focus();
-    }
-
-    // If backspace is used and text is deleted, focus on the previous input
-    if (!text && index > 0) {
-      inputRefs.current[index - 1].focus();
-    }
+  const handleOTPChange = (text) => {
+    setOtp(text);
   };
 
-  // Autofocus on the first input on initial render
-  useEffect(() => {
-    inputRefs.current[0].focus();
-  }, []);
+  const otpBoxes = Array.from({ length: 6 }).map((_, index) => {
+    const digit = otp[index] || '';
+    const isFocused = index === otp.length;
+    const boxStyle = isFocused ? styles.highlightedBox : styles.otpInput;
+
+    return (
+      <Pressable key={index} style={boxStyle} onPress={() => inputRef.current.focus()}>
+        <Text style={styles.otpText}>{digit}</Text>
+      </Pressable>
+    );
+  });
 
   return (
     <SafeAreaView style={styles.container}>
       <View style={styles.innerContainer}>
-      <View style={styles.header}>
-      
-      <Image source={OTPImage} style={styles.image} resizeMode="contain" />
-      <View style={styles.textContainer}>
-        <Text style={styles.headerText}>OTP Verification</Text>
-        <Text style={styles.promptText}>Enter the code sent to +1 999 888...</Text>
-        </View>
+        <View style={styles.header}>
+          <Image source={OTPImage} style={styles.image} resizeMode="contain" />
+          <View style={styles.textContainer}>
+            <Text style={styles.headerText}>OTP Verification</Text>
+            <Text style={styles.promptText}>Enter the code sent to +1 999 888...</Text>
+          </View>
         </View>
         <View style={styles.otpContainer}>
-          {otp.map((digit, index) => (
-            <TextInput
-              key={index}
-              ref={(ref) => inputRefs.current[index] = ref}
-              style={styles.otpInput}
-              keyboardType="number-pad"
-              value={digit}
-              onChangeText={(text) => handleOTPChange(text, index)}
-              maxLength={1}
-              placeholderTextColor="#D3D3D3"
-            />
-          ))}
+          {otpBoxes}
         </View>
+        <TextInput
+          ref={inputRef}
+          style={styles.hiddenInput}
+          keyboardType="number-pad"
+          value={otp}
+          onChangeText={handleOTPChange}
+          maxLength={6}
+          autoFocus
+        />
         <Button
           title="Verify"
-          onPress={() => navigation.navigate(PAGES.SIGN_UP)} // Update with the actual navigation
+          onPress={() => navigation.navigate(PAGES.SIGN_UP)} // Update with actual navigation
         />
         <Text style={styles.resendText}>Didn’t receive the code? Resend</Text>
       </View>
@@ -76,6 +72,16 @@ const styles = StyleSheet.create({
     width: '100%',
     paddingHorizontal: calcWidth(5),
     alignItems: 'center',
+  },
+  header: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    margin: calcWidth(5),
+  },
+  textContainer: {
+    flex: 1,
+    justifyContent: 'flex-end',
+    height: calcHeight(10),
   },
   headerText: {
     fontSize: getFontSizeByWindowWidth(18),
@@ -100,6 +106,25 @@ const styles = StyleSheet.create({
     fontSize: calcWidth(5),
     color: COLOR.TEXT,
   },
+  highlightedBox: {
+    width: calcWidth(10),
+    borderBottomWidth: 2,
+    borderColor: COLOR.PRIMARY,
+    textAlign: 'center',
+    fontSize: calcWidth(5),
+    color: COLOR.TEXT,
+  },
+  otpText: {
+    fontSize: getFontSizeByWindowWidth(20),
+    color: COLOR.TEXT,
+    marginBottom:calcHeight(2)
+  },
+  hiddenInput: {
+    position: 'absolute',
+    width: 1,
+    height: 1,
+    opacity: 0,
+  },
   resendText: {
     color: COLOR.PRIMARY,
     fontSize: calcWidth(3.5),
@@ -109,16 +134,6 @@ const styles = StyleSheet.create({
     width: calcWidth(20),
     height: calcHeight(20),
     marginRight: calcWidth(5),
-  },
-  header: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    margin: calcWidth(5),
-  },
-  textContainer: {
-    flex: 1,
-    justifyContent: 'flex-end',
-    height: calcHeight(10),
   }
 });
 
