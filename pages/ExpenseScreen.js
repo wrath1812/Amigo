a1fa8e693aca1ae925bad85861f4ab17486840d2
Bhaumik-Tandan <bhaumik.tandan@gmail.com>
2024-01-02 17:49:46 +0530

    [ADD] expense screen

---
+++ pages/ExpenseScreen.js
@@ -0,0 +1,132 @@
+import React, { useState, useEffect, useCallback } from 'react';
+import {
+    StyleSheet,
+    SafeAreaView,
+    View,
+    Text,
+    FlatList,
+    Image,
+    Pressable
+} from 'react-native';
+import Loader from '../components/Loader';
+import apiHelper from '../helper/apiHelper';
+import { useFocusEffect } from '@react-navigation/native';
+import COLOR from '../constants/Colors';
+import { calcHeight, calcWidth,getFontSizeByWindowWidth } from '../helper/res';
+import { useAuth } from '../context/AuthContext';
+import { Ionicons } from '@expo/vector-icons';
+import GroupIcon from '../components/GroupIcon';
+function convertISODateToCustomFormat(isoDate) {
+    const date = new Date(isoDate);
+    const day = date.getDate().toString().padStart(2, '0');
+    const month = (date.getMonth() + 1).toString().padStart(2, '0'); // Months are zero-indexed
+    const year = date.getFullYear();
+    const hours = date.getHours();
+    const minutes = date.getMinutes().toString().padStart(2, '0');
+    const ampm = hours >= 12 ? 'PM' : 'AM';
+    const formattedHour = hours % 12 || 12; // Convert 24h to 12h format and treat 0 as 12
+    return `${day}/${month}/${year} at ${formattedHour}:${minutes} ${ampm}`;
+  }
+  
+  
+function ExpenseScreen({ navigation }) {
+    const [loading, setLoading] = useState(false);
+    const [transactions, setTransactions] = useState([]);
+    const { user } = useAuth();
+
+    useFocusEffect(
+        useCallback(() => {
+            (async () => {
+                setLoading(true);
+                try {
+                    const { data } = await apiHelper('/transaction/expenses');
+                    setTransactions(data);
+                } catch (error) {
+                    console.error(error);
+                } finally {
+                    setLoading(false);
+                }
+            })();
+        }, [user.id]),
+    );
+
+    return (
+        <SafeAreaView style={styles.container}>
+            <Text style={styles.header}>Expense Summary</Text>
+
+            {loading ? (
+                <Loader />
+            ) : transactions.length === 0 ? (
+                <Text style={styles.noTransactionsText}>No Transactions Found</Text>
+            ) : (
+                <FlatList
+                    data={transactions}
+                    keyExtractor={(item, index) => item.id || index.toString()}
+                    renderItem={({ item }) => (
+                        <View style={{
+                            flexDirection:"row",
+                            paddingVertical:calcHeight(1),
+                            justifyContent:"space-around",
+                            borderBottomWidth:1,
+                            borderBottomColor: "rgba(255, 255, 255, 0.13)",
+                            alignItems:"center",
+                        }}>
+                            <View style={{
+                            flexDirection:"row",
+                            paddingVertical:calcHeight(1),
+                            justifyContent:"space-around",
+                            alignItems:"center",
+                            width:"50%"
+
+                        }}>
+                                <View>
+                            <GroupIcon size={{
+                                width:calcHeight(5),
+                                height:calcHeight(5)
+                            }}/>
+                            </View>
+                            <View style={{
+                                gap:calcHeight(1)
+                            }}>
+                                <Text style={{
+                                    color:COLOR.BUTTON,
+                                    fontWeight:"bold",
+                                    fontSize:getFontSizeByWindowWidth(15)
+                                }}>{item.description}</Text>
+                                <Text style={{
+                                    color:"white"
+                                }}>{item.group}</Text>
+                                <Text style={{
+                                    color:"white",
+                                    fontSize:getFontSizeByWindowWidth(8)
+                                }}>{convertISODateToCustomFormat(item.date)}</Text>
+                                </View>
+                                </View>
+                                <Text style={{
+                                    color:"white",
+                                    fontSize:getFontSizeByWindowWidth(15)
+                                }}>${item.amount}</Text>
+                        </View>
+                    )}
+                    style={styles.list}
+                />
+            )}
+        </SafeAreaView>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: COLOR.APP_BACKGROUND,
+    },
+    header: {
+        fontSize: getFontSizeByWindowWidth(19),
+        color: COLOR.TEXT,
+        fontWeight: 'bold',
+        margin:calcHeight(5)
+    }
+    // ... other styles
+});
+
+export default ExpenseScreen;
