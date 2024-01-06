e993b4f69e0c4dc81a974fc53bbfc0162c7afca0
Bhaumik-Tandan <bhaumik.tandan@gmail.com>
2024-01-06 11:58:16 +0530

    [ADDD] checkconnectivity function

--- helper/apiHelper.js
+++ helper/apiHelper.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { API_URL } from '@env';
 import { TOKEN } from '../constants/string';
 import { getLocalStoreData } from './localStorage';
-
+import getNetworkStateAsync from "./getNetworkStateAsync";
 const config = {
     baseURL: API_URL,
     timeout: 10000, // env value is a string so we need to convert it to a number
---
+++ helper/getNetworkStateAsync.js
@@ -0,0 +1,9 @@
+import * as Network from 'expo-network';
+
+async function checkConnectivity() {
+  const networkState = await Network.getNetworkStateAsync();
+  return networkState.isInternetReachable;
+}
+
+
+export default checkConnectivity;
