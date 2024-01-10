9e9bb24db2626122ecc8cd0852e251fd539c1616
Bhaumik-Tandan <bhaumik.tandan@gmail.com>
2024-01-11 02:00:43 +0530

    [ADD[ account transaction

---
+++ components/AccountPageOption.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { StyleSheet, Text, Pressable } from 'react-native';
+import { calcHeight, calcWidth, getFontSizeByWindowWidth } from '../helper/res';
+
+function AccountPageOption({
+    iconName,
+    label,
+    IconComponent,
+    additionalStyle,
+    onPress,
+}) {
+    return (
+        <Pressable
+            style={[styles.menuOption, additionalStyle]}
+            onPress={onPress}
+        >
+            <IconComponent name={iconName} size={calcHeight(3)} color="white" />
+            <Text style={styles.menuText}>{label}</Text>
+        </Pressable>
+    );
+}
+
+const styles = StyleSheet.create({
+    menuOption: {
+        flexDirection: 'row',
+        margin: calcWidth(5),
+        alignItems: 'center',
+        gap: calcWidth(10),
+    },
+    menuText: {
+        color: 'white',
+        fontWeight: 'bold',
+    },
+});
+
+export default AccountPageOption;
