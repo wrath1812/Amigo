2f265ab1a462f401dc592f341639a88de14495c3
Bhaumik-Tandan <bhaumik.tandan@gmail.com>
2024-01-14 20:03:33 +0530

    [ADD[ about screen

---
+++ constants/about.js
@@ -0,0 +1,3 @@
+const about=`Amigo is a is a new-age fintech app that powers group payments. Reimagine group payments and enable people to enjoy with their friends without any worry of awkward reminders, delayed payments, and multiple transactions.`;
+
+export default about;
