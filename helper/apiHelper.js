6d7ab40517fe0fc1bc87e3393ee3fa1f6120651e
Bhaumik-Tandan <bhaumik.tandan@gmail.com>
2024-01-22 09:18:32 +0530

    [EDIT] api helper

--- helper/apiHelper.js
+++ helper/apiHelper.js
@@ -1,52 +1,29 @@
 import axios from 'axios';
 import { API_URL } from '@env';
 import { TOKEN } from '../constants/string';
-import { getLocalStoreData, setLocalStoreData } from './localStorage';
-import getNetworkStateAsync from './getNetworkStateAsync';
+import { getLocalStoreData } from './localStorage';
 
 const config = {
     baseURL: API_URL,
-    timeout: 10000,
+    timeout: 10000, // env value is a string so we need to convert it to a number
     headers: {
         'Content-Type': 'application/json',
     },
 };
-
 const request = axios.create(config);
-
 request.interceptors.request.use(
     async (reqConfig) => {
-        const isOnline = await getNetworkStateAsync();
-
-        if (!isOnline) {
-            // Get data from local storage if offline
-            const localData = await getLocalStoreData(reqConfig.url);
-            if (localData) {
-                return Promise.resolve({ data: localData, fromCache: true });
-            }
-        }
-
-        // Online scenario: Add token to header
+        const reqHeader = { ...reqConfig };
         const token = await getLocalStoreData(TOKEN);
         if (token) {
-            reqConfig.headers = {
+            reqHeader.headers = {
                 ...reqConfig.headers,
                 Authorization: `Bearer ${token}`,
             };
         }
 
-        return reqConfig;
+        return reqHeader;
     },
     (error) => Promise.reject(error),
 );
-
-request.interceptors.response.use(
-    async (response) => {
-        // Store response data in local storage
-        setLocalStoreData(response.config.url, response.data);
-        return response;
-    },
-    (error) => Promise.reject(error),
-);
-
 export default request;
