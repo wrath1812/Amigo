9e9a4cb9edaf536726b14f35a13d1a29fdb6ccb3
Bhaumik-Tandan <bhaumik.tandan@gmail.com>
2024-02-24 18:36:37 +0530

    [rfiz] CODE FOR SYNC CHAT

--- pages/Group.js
+++ pages/Group.js
@@ -89,6 +89,8 @@ function GroupScreen({ navigation }) {
     }, []);
 
     const fetchActivities = async () => {
+        const isOnline = await checkConnectivity();
+        if(!isOnline) return;
         await syncAllChat();
         try {
             const { data } = await apiHelper(
