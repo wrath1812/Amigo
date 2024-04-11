3435f366bcfe2ccf595dd51a5e9c84e4afc44674
Bhaumik-Tandan <bhaumik.tandan@gmail.com>
2024-04-12 01:16:01 +0530

    [FIX] check selecred when group added

--- components/ContactCard.js
+++ components/ContactCard.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { StyleSheet, View, Text, Image } from 'react-native';
-import { Ionicons, MaterialCommunityIcons } from '@expo/vector-icons'; // Make sure to install expo-icons or another icon library
+import { Ionicons, MaterialCommunityIcons,AntDesign } from '@expo/vector-icons'; // Make sure to install expo-icons or another icon library
 
 // Helper functions for responsive layout
 import { calcHeight, calcWidth, getFontSizeByWindowWidth } from '../helper/res';
@@ -22,7 +22,7 @@ function ContactCard({ selected, color, name, phoneNumber, imageURI }) {
             </View>
             <View style={styles.selectorContainer}>
                 {selected ? (
-                    <Ionicons name="md-checkmark-circle" size={calcWidth(5)} color={COLOR.BUTTON} />
+                    <AntDesign name="checkcircle" size={calcWidth(5)} color={COLOR.BUTTON} />
                 ) : (
                     <MaterialCommunityIcons name="checkbox-blank-circle-outline" size={calcWidth(5)} color="white" />
                 )}
